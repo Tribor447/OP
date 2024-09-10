e53d983b01a8ca8867d51cad778bae413acc5c41
Tribor447 <6595932@gmail.com>
2024-09-10 12:27:44 +0300

    Выполнил Task1.1

--- hello-dotnet/hello-dotnet/Program.cs
+++ hello-dotnet/hello-dotnet/Program.cs
@@ -1,1 +1,12 @@
-﻿Console.WriteLine("Hello, World!");
+﻿using System;
+public class Task1{
+    public static void Main(){
+        sayHello("Илья");
+
+    }
+    public static void sayHello(string name){
+        string sms=$"Hello {name}!";
+        Console.WriteLine(sms);
+    }
+}
+
